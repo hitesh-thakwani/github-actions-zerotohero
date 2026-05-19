ca11d570db77c53d7e65650622075c730c098461
Hitesh Thakwani <hiteshthakwani@ymail.com>
2026-05-19 15:04:15 +0530

    updated app file

--- app.py
+++ app.py
@@ -11,5 +11,4 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+app.run(debug=True, host='0.0.0.0', port=80)
